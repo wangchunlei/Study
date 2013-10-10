52811ae4308f1478cfdb3d18bb16064ae51c3ebb
wangchunlei <wcl_orange@163.com>
2013-10-10 10:18:58 +0800

    culture and resources
    [assembly: NeutralResourcesLanguage("en",
    UltimateResourceFallbackLocation.MainAssembly)]

--- asp.net mvc/Domas.Web.Mvc/Properties/AssemblyInfo.cs
+++ asp.net mvc/Domas.Web.Mvc/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Security;
 [assembly: AssemblyTitle("Domas.Web.Mvc")]
 [assembly: AssemblyProduct("Domas.Mvc.Web")]
 [assembly: AssemblyDescription("Domas Web Extensions for ASP.NET MVC")]
-[assembly: NeutralResourcesLanguage("zh-CN")]
+[assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
 [assembly: InternalsVisibleTo("Kendo.Mvc.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010069f31f753c891d9f7f9d1cd0b579f61977769a5a48e01247cc2481c8765613854c8dcb5101dd092d5075a7339b4e34d0c9bd417f54972c7554ae480d6b1bb17be8c1527554644bbd352d9498b174eaff1090a30e1f7c2c3073669cb3efc7d9640e82049f5fda08ca58072c14169091a0bc7092eb6de9c2a249a3c80f7704e5cf")]
--- asp.net mvc/Examples/Models/StockData.designer.cs
+++ asp.net mvc/Examples/Models/StockData.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kendo.Mvc.Examples.Models
+namespace Domas.Web.Mvc.Examples.Models
 {
 	using System.Data.Linq;
 	using System.Data.Linq.Mapping;
