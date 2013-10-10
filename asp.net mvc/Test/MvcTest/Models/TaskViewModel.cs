4e295237696b2cbe46a6c6f09385feef1a291294
wangchunlei <wcl_orange@163.com>
2013-10-10 17:24:38 +0800

    commit

---
+++ asp.net mvc/Test/MvcTest/Models/TaskViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Domas.Web.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class TaskViewModel : ISchedulerEvent
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        private DateTime start;
+        public DateTime Start
+        {
+            get
+            {
+                return start;
+            }
+            set
+            {
+                start = value.ToUniversalTime();
+            }
+        }
+
+        public string StartTimezone { get; set; }
+
+        private DateTime end;
+        public DateTime End
+        {
+            get
+            {
+                return end;
+            }
+            set
+            {
+                end = value.ToUniversalTime();
+            }
+        }
+
+        public string EndTimezone { get; set; }
+
+        public string RecurrenceRule { get; set; }
+        public int? RecurrenceID { get; set; }
+        public string RecurrenceException { get; set; }
+        public bool IsAllDay { get; set; }
+        public int? OwnerID { get; set; }
+    }
+}
