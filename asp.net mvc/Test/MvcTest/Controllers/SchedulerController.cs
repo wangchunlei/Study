59205de43e02648280a0d4d825d1c05c4e4e8622
wangchunlei <wcl_orange@163.com>
2013-10-17 17:52:06 +0800

    commit

--- asp.net mvc/Test/MvcTest/Controllers/SchedulerController.cs
+++ asp.net mvc/Test/MvcTest/Controllers/SchedulerController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Domas.Web.Mvc.Extensions;
 
 namespace MvcTest.Controllers
 {
@@ -21,7 +22,24 @@ namespace MvcTest.Controllers
         public virtual JsonResult Read([DataSourceRequest] DataSourceRequest request)
         {
             //return Json(taskService.GetAll().ToDataSourceResult(request));
-            return null;
+            var tasks = new List<TaskViewModel>();
+            tasks.Add(new TaskViewModel()
+            {
+                TaskID=1,Description="2小时内租用0021车",OwnerID=2,Start=DateTime.Now,End=DateTime.Now.AddHours(2),Title="租车"
+            });
+
+            tasks.Add(new TaskViewModel()
+            {
+                TaskID = 2,
+                Description = "下午2点 -- 4点占用会议室 213",
+                OwnerID = 2,
+                Start = DateTime.Now.Date.AddHours(14),
+                End = DateTime.Now.Date.AddHours(16),
+
+                Title = "申请会议室"
+            });
+
+            return Json(tasks.ToDataSourceResult(request));
         }
 
         public virtual JsonResult Destroy([DataSourceRequest] DataSourceRequest request, TaskViewModel task)
