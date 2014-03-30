1109a1043b0aab2f593a181ceac0cdeed4b58842
wangchunlei <wcl_orange@163.com>
2014-03-30 20:19:09 +0800

    sqlcompact

---
+++ Labs/EF4SqlCompact/EF4SqlCompact/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF4SqlCompact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ModelContext())
+            {
+                var model = new Model()
+                {
+                    Code = "wangcl",
+                    Name = "王春雷"
+                };
+
+                context.Models.Add(model);
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class Model
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class ModelContext : DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+    }
+}
